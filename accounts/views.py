a3e08b7aa8e5424c0e1249208fcdcb4259fe9b71
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-01-20 20:21:37 +0100

    paused

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def register(request):
+
+    if request.method == 'POST':
+        firstname = request.POST['first_name'],
+        lastname = request.POST['last_name'],
+        username = request.POST['username'],
+        email = request.POST['email'],
+        password = request.POST['password'],
+        confirmPassword = request.POST['confirmpassword'],
+
     return render(request, 'register.html')
 
 def login(request):
